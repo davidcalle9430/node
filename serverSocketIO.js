5d54982546e09712df50534cfa57c26099ccf0b3
David Calle <davidcalle9430@gmail.com>
2015-04-28 17:16:17 -0500

    Prueba 1

--- serverSocketIO.js
+++ serverSocketIO.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log( "el usario "+this.socket.sessionid + ' envía message: ' + msg);
+    console.log( "el usario "+ socket.id + ' envía message: ' + msg);
     io.emit('chat message', msg);
   });
 });
