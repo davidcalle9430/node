62d6b66ed31400a08676f079d290fbb52e244d0b
David Calle <davidcalle9430@gmail.com>
2015-04-28 16:22:17 -0500

    Prueba 1

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var server = require('websocket').server, http = require('http');
-  var cantidad=0;
+var server = require('websocket').server,
+    http = require('http');
+var cantidad=0;
 var socket = new server({
     httpServer: http.createServer().listen(1337)
 });
 
 socket.on('request', function(request) {
     var connection = request.accept(null, request.origin);
+    console.log(request.origin);
     connection.on('message', function(message) {
         console.log(message.utf8Data);
         connection.sendUTF('hello');
@@ -13,6 +15,9 @@ socket.on('request', function(request) {
             connection.sendUTF('Hola, eres el usuario '+ cantidad);
             cantidad++;
         }, 1000);
+
+
+
     });
 
     connection.on('close', function(connection) {
