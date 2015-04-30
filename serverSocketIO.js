f9442bffd3bd7ebabc3f330319df8ce5db2b7e73
David Calle <davidcalle9430@gmail.com>
2015-04-30 14:27:38 -0500

    user test

--- serverSocketIO.js
+++ serverSocketIO.js
@@ -1,14 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var clients =[];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname +'/index.html');
 });
 
 io.on('connection', function(socket){
+
+	console.log("usuarios actuales");
+	for (var i in clients) {
+		console.log(i);
+	};
+
   socket.on('chat message', function(msg){
-    console.log( "el usario "+ socket.id + ' envía message: ' + msg);
+  	clients.push(socket.index);
+    console.log( "el usuario "+ socket.id + ' envía message: ' + msg);
     io.emit('chat message', msg);
   });
 });
