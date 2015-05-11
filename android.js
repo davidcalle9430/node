77778cb2efd5b343dfd70d3a8e77e9d4941fea98
David Calle <davidcalle9430@gmail.com>
2015-05-10 19:43:05 -0500

    fix socket error

--- android.js
+++ android.js
@@ -106,7 +106,7 @@ io.on('connection', function (socket) {
     delete clients[username];
     delete activeSockets[socket.id];
     console.log("prueba de borrado "+ clients[username]+ "-"+activeSockets[socket.id]);
-    io.broadcast.emit('disconnect',{user:username});
+    sockets.broadcast.emit('disconnect',{user:username});
   });
 
   socket.on('start_session', function(msg){
