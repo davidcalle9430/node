d20076e193312a13bb3fee296eb1f597b3160396
David Calle <davidcalle9430@gmail.com>
2015-05-31 15:01:14 -0500

    Fix return

--- android.js
+++ android.js
@@ -118,7 +118,7 @@ io.on('connection', function (socket) {
     m= m+ "%";
     console.log("Got a hint "+m);
     pool.getConnection(function(err, connection){
-        connection.query('SELECT p.username, ph.idPhoto FROM (Pet p INNER JOIN User o ON p.username = o.username) LEFT JOIN Photo ph ON ph.idPhoto =o.idPhoto_profile  WHERE p.username  LIKE ?', [m], function(err, results) {
+        connection.query('SELECT p.username, ph.path FROM (Pet p INNER JOIN User o ON p.username = o.username) LEFT JOIN Photo ph ON ph.idPhoto =o.idPhoto_profile  WHERE p.username  LIKE ?', [m], function(err, results) {
             console.log(results);
             io.to(socket.id).emit('hint', {result:results});
         });
