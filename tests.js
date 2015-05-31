dc6e5fb4b17b5154f0fb30702c786c79d97e3249
David Calle <davidcalle9430@gmail.com>
2015-05-31 15:31:14 -0500

    TEST

--- tests.js
+++ tests.js
@@ -7,7 +7,7 @@ var options ={
   'force new connection': true
 };
 
-var chatUser1 = {'username':'TorredM'};
+var chatUser1 = {'username':'TorresM'};
 var chatUser2 = {'username':'JaimeP'};
 var chatUser3 = {'username':'AlfredoS'};
 
@@ -45,16 +45,59 @@ describe("doAsync",function(){
 	  var client1 = io.connect(socketURL, options);
 	  var client2 = io.connect(socketURL, options);
 	  
-		client1.on('response_start_session', function(data1){
+		client1.on('chat_message', function(data1){
 	  	 		var connectedUser = JSON.parse(data1);
-	  			connectedUser.users[0].should.equal(chatUser2['username']);
-				connectedUser.users[0].should.equal("JaimeP");
+	  			connectedUser['sender'].should.equal('JaimeP');		
 	 	});
-	 	client2.emit('start_session', chatUser2);
+	 	var message ={sender:'JaimeP', receiver: 'TorresM'};
+	 	client2.emit('start_session',message );
 	 	done();
 
 	});
 
+	/*
+	* 
+	* Prueba unitaria 3, 
+	* Debe Enviar y Recibir Notificaciones al usuario
+	* 
+	*/
+
+	it('Should send and receive notifications', function(done){
+	  var client1 = io.connect(socketURL, options);
+	  var client2 = io.connect(socketURL, options);
+	  
+		client1.on('notification', function(data1){
+	  	 		var connectedUser = JSON.parse(data1);
+	  			connectedUser[0].username.should.equal('pongo');		
+	 	});
+	 	var message ={sender:'JaimeP', receiver: 'TorresM'};
+	 	client2.emit('notification',message );
+	 	done();
+
+	});
+	/*
+	*
+	* 
+	* Prueba unitaria 3, 
+	* Debe dar pistas sobre el username enviado
+	* 
+	*/
+
+	it('Should send and receive notifications', function(done){
+	  var client1 = io.connect(socketURL, options);
+	  var client2 = io.connect(socketURL, options);
+	  
+		client1.on('hint', function(data1){
+	  	 		var connectedUser = JSON.parse(data1);
+	  			console.log(connectedUser);
+	  			//connectedUser['sender'].should.equal('JaimeP');		
+	 	});
+	 	// falta mockear 
+	 	var message ={username:'pon'};
+	 	client2.emit('hint',message );
+	 	done();
+
+	});
 
 
 
